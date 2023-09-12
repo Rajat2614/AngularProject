d8664a507887cec6a6aec25698eac41b982daba8
RAJAT CHAUHAN <98510021+Rajat2614@users.noreply.github.com>
2023-09-12 10:46:21 +0530

    Add files via upload

---
+++ AngProject/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,31 @@
+import { OnDestroy } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+
+import { Post } from "../post.model";
+import { PostsService } from "../posts.service";
+
+@Component({
+  selector: "app-post-list",
+  templateUrl: "./post-list.component.html",
+  styleUrls: ["./post-list.component.css"],
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.postsService.getPosts();
+    this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.postsSub) {
+      this.postsSub.unsubscribe();
+    }
+  }
+}
---
+++ AngProject/src/app/posts/posts.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { Post } from "./post.model";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PostsService {
+  constructor(private http: HttpClient) {}
+
+  private posts: Post[] = [];
+  private postUpdated = new Subject<Post[]>();
+
+  getPosts() {
+    this.http
+      .get<{ message: string; posts: Post[] }>(
+        "http://localhost:3000/api/posts"
+      )
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postUpdated.next([...this.posts]);
+      });
+  }
+  getPostUpdateListener() {
+    return this.postUpdated.asObservable();
+  }
+
+  addPost(title: string, content: string) {
+    const post: Post = { id: null, title: title, content: content };
+    this.posts.push(post);
+    this.postUpdated.next([...this.posts]);
+  }
+}
